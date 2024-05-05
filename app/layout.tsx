bf37b8ea508dccfb7f9bcdf7b156897d57feef95
Yuanlin Lin <im.yuanlinlin@gmail.com>
2024-05-05 17:29:46 +0800

    add banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,12 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
   return <html lang="zh-TW">
   <body>
-  <div className="flex h-[100svh] w-screen items-center justify-center" style={{
-    backgroundImage: "url('/bg.png')",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  }}>
+  <div className="flex h-[100svh] w-screen items-center justify-center">
+    <img
+      src="/banner.webp"
+      className="absolute -top-48 z-10 w-[800px]"
+      alt=""
+    />
     <div
       className="relative z-20 h-[100svh] w-screen overflow-hidden bg-gray-3 shadow-2xl sm:h-[672px] sm:w-[402px] md:rounded-lg">
       <div className="h-full overflow-y-scroll">
