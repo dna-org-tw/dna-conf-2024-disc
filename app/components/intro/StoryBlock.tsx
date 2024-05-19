4c94b128abc8f1544268cdb29dffeab70ef2ba2e
loischen68 <a8283626@gmail.com>
2024-05-20 05:35:17 +0800

    feat: StoryBlock component and PlayerBlock component

---
+++ app/components/intro/StoryBlock.tsx
@@ -0,0 +1,11 @@
+export default function StoryBlock() {
+  return (
+    <div className="flex flex-col gap-1 items-center p-6 rounded-3xl bg-white bg-opacity-60">
+      <p>
+        西元 3038
+        年，世界充斥怪物，冒險者公會作為人族最後淨土，組建團隊只為守護世界！
+      </p>
+      <p className="text-[#E4003D] text-lg font-bold">你會是哪一種冒險者呢？</p>
+    </div>
+  );
+}
